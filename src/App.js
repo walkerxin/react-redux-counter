ee22c191dbe80e0c7847bca7bcb1fb66dc59ac04
walkerxin <1464193658@qq.com>
2020-09-05 23:55:27 +0800

    Redux + React 实现一个 Counter
    功能：add1 add2 addIfOdd addAfterTwoSecond

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	addIfOdd() {
+		console.log('here', this.props.value)
+		if (this.props.value % 2 !== 0) {
+			this.props.add1()
+		}
+	}
+	addAfterTwoSecond() {
+		setTimeout(() => this.props.add2(), 2000)
+	}
+	render() {
+		return (
+			<div className="App">
+			你点击了<span>{this.props.value}</span>次
+			<button onClick={this.props.add1}>+1</button>
+			<button onClick={this.props.add2}>+2</button>
+			<button onClick={() => this.addIfOdd()}>单数就 +1</button>
+			<button onClick={() => this.addAfterTwoSecond()}>两秒后 +2</button>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function stateChanger(state, action) {
+	if (state === undefined) {
+		return 0
+	}
+	if (action.type === 'add') {
+		const newState = state + (action.payload || 1)
+		return newState
+	} else {
+		return state
+	}
+}
+const store = createStore(stateChanger)
+
+const render = () => ReactDOM.render(
+	<React.StrictMode>
+		<App value={store.getState()}
+			add1={() => store.dispatch({ type: 'add', payload: 1 })}
+			add2={() => store.dispatch({ type: 'add', payload: 2 })}
+		/>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
+render()
+store.subscribe(render)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
